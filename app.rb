8505958de361a957462fb7fc344ab27edd3d8ed1
audrie <audrie.talbot@gmail.com>
2015-05-15 11:00:21 -0700

    Display lists of bands and venuesand allows user to add each to db.

--- app.rb
+++ app.rb
@@ -16,7 +16,33 @@ get('/bands') do
   erb(:bands)
 end
 
+post('/bands') do
+  name = params.fetch('name')
+  band = Band.create({:name => name})
+  @bands = Band.all()
+  erb(:bands)
+end
+
 get('/venues') do
   @venues = Venue.all()
   erb(:venues)
+end
+
+post('/venues') do
+  name = params.fetch('name')
+  venue = Venue.create({:name => name})
+  @venues = Venue.all()
+  erb(:venues)
+end
+
+get('/bands/:id') do
+  @band = Band.find(params.fetch('id').to_i())
+  @venues = Venue.all()
+  erb(:band_info)
+end
+
+get('/venues/:id') do
+  @venue = Venue.find(params.fetch('id').to_i())
+  @bands = Band.all()
+  erb(:venue_info)
 end
