a105687bb67f829907162f8fc75b82f7c6a63e0f
audrie <audrie.talbot@gmail.com>
2015-05-15 10:12:09 -0700

    Display index page with links.

--- app.rb
+++ app.rb
@@ -3,3 +3,20 @@ require('pry')
 Bundler.require(:default, :production)
 
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
+
+
+get('/') do
+  @bands = Band.all()
+  @venues = Venue.all()
+  erb(:index)
+end
+
+get('/bands') do
+  @bands = Band.all()
+  erb(:bands)
+end
+
+get('/venues') do
+  @venues = Venue.all()
+  erb(:venues)
+end
